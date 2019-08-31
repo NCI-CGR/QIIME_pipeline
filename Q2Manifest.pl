57a2a3a75b808de2fe2e0a90981d9ed444e186c7
Samantha Chill <samantha.sevilla@nih.gov>
2019-08-31 10:31:38 -0400

    take in full manifest path from cml

--- Q2Manifest.pl
+++ Q2Manifest.pl
@@ -2,28 +2,25 @@
 use strict;
 use warnings;
 
-@ARGV==2 or die "wrong number of arguments";
+@ARGV==3 or die "wrong number of arguments";
 
-#Create QIIME2 manifest
+# QIIME2 manifest
 my $project_dir =$ARGV[0];
-my $manifest_ori=$ARGV[1];
+my $manifest_fullpath=$ARGV[1];
+my $manifest_input=$ARGV[2];
 
-manifest_qiime2 ($project_dir,$manifest_ori);
+manifest_qiime2 ($project_dir,$manifest_fullpath, $manifest_input);
 
 sub manifest_qiime2{
 	#Initialize variables / Read in variables
-	my ($project_dir, $manifest_ori)=@_;
-
-	#Set pathway for original manifest
-	my $manifest_path=$project_dir; $manifest_path.="//";
-	$manifest_path.= $manifest_ori;
+	my ($project_dir, $manifest_fullpath, $manifest_input)=@_;
 
 	#Set pathway and name for qiime2 file
 	my $MANIFEST_FILE_QIIME = $project_dir; $MANIFEST_FILE_QIIME .="//manifest_qiime2.tsv";
 	open my $fh, ">$MANIFEST_FILE_QIIME";
 
 	#Open, read original manifest file
-	open my $in, "<:encoding(UTF-8)", $manifest_path or die "$manifest_path: $!";
+	open my $in, "<:encoding(UTF-8)", $manifest_fullpath or die "$manifest_fullpath: $!";
 	my @lines = <$in>; close $in;
 	chomp @lines;
 
