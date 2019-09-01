3ad13649044eb4fc0452d91f16cf4939015b323f
Samantha Chill <samantha.sevilla@nih.gov>
2019-09-01 01:09:45 -0400

    update file creation with file checking

--- Q2Manifest.pl
+++ Q2Manifest.pl
@@ -6,18 +6,17 @@ use warnings;
 #output
 #usage
 
-@ARGV==4 or die "wrong number of arguments";
+@ARGV==3 or die "wrong number of arguments";
 
 my $project_dir =$ARGV[0];
 my $manifest_fullpath=$ARGV[1];
-my $manifest_input=$ARGV[2];
-my $manifest_output=$ARGV[3];
+my $manifest_output=$ARGV[2];
 
-my @lines;
+my @lines; my $flag=0;
 
 read_manifest ($manifest_fullpath, \@lines);
 check_input(\@lines);
-create_manifest($project_dir,$manifest_output,\@lines);
+create_manifest($manifest_output,\@lines);
 
 sub read_manifest{
 	my ($manifest_fullpath, $lines)=@_;
@@ -28,7 +27,7 @@ sub read_manifest{
 }
 
 sub check_input{
-	my ($lines)=@_;
+	my ( $lines)=@_;
 	my $n=1;
 	my @header; my @sampleids; my @metavalues;
 	my %seen;
@@ -42,7 +41,7 @@ sub check_input{
 			}
 		} else{
 			push(@sampleids,$rows[0]); shift @rows;
-			foreach (@rows){
+			foreach (@rows){ #may not be needed unless we want to check cat vs num?
 				push(@metavalues,$_);
 			};
 		};
@@ -53,7 +52,6 @@ sub check_input{
 	#If any other value found, error for the user to check the manifest re-try
 	if($header[0] ne "#SampleID"){
 		print "First column must be #SampleID - check file and re-submit";
-		exit;
 	}
 
 	#Sample IDs must be unique or the manifest will not be accepted.
@@ -71,19 +69,18 @@ sub check_input{
 		print join( ',', @duplicates); print"\n\n";
 	}
 
-
 	#All other cell rules
 }
 
 sub create_manifest{
-	my ($project_dir, $manifest_output, $lines)=@_;
+	my ($manifest_output, $lines)=@_;
 
 	open my $fh, ">$manifest_output";
 
 	foreach (@lines) {
-		my @columns = split('\t',$_);
-#print "or here @columns\n\n";
 		my $n=0;
+		my @columns = split('\t',$_);
+
 		until ($n+1 > scalar(@columns)){
 			print $fh "$columns[$n]\t";
 			$n++;
