de7ff5e1df220c3b6db1b8e90723b78bb6ab0194
Samantha Chill <samantha.sevilla@nih.gov>
2019-09-02 14:25:08 -0400

    problems with snakemake on create_symlinks

--- CreateSymlinks.pl
+++ CreateSymlinks.pl
@@ -3,28 +3,26 @@ use strict;
 use warnings;
 use Cwd;
 
-
 #input
-##Requires 3 CML arguements -
-##1) full path to project directory
-##2) full path to the manifest file
-##3) full path to output file
+##Requires 2 CML arguements -
+##1) full path to split manifest files
+##2) full path to the output files
 
 #output
-##QIIME2 TSV formatted metadata file, in /path/to/projectdirectory/Input
+##symlinks for fasta files in split fasta directories
 
 @ARGV==2 or die "
 Usage: $0 /path/to/splitmanifest /path/to/splitfastaoutputdir";
 
-my $manifest_file_split_fastq =$ARGV[0];
+my $manifest_file_split_fastq_dir =$ARGV[0];
 my $fastq_dir=$ARGV[1];
 
-create_symlinks($manifest_file_split_fastq,$fastq_dir);
+create_symlinks($manifest_file_split_fastq_dir,$fastq_dir);
 
 ######################################################################################
 								##Subroutines##
 ######################################################################################
-sub create_symlinks{
+sub create_symlinks2{
 	my ($manifest_file_split_fastq,$fastq_dir)=@_;
 	#Open split meta file
 	open my $fh, "<:encoding(utf8)", $manifest_file_split_fastq or die "$manifest_file_split_fastq: $!";
@@ -39,10 +37,39 @@ sub create_symlinks{
 
 		my $file_name = $location_old =~ /(SC[-_0-9A-Z]+001)/; $file_name=$1;
 		my $location_new = "$fastq_dir/$file_name.fastq.gz";
-		#$file_name .= ".fastq.gz";
 
 		print "OLD: $location_old\n";
 		print "NEW: $location_new\n\n";
+		symlink($location_old, $location_new);
+	}
+}
+
+sub create_symlinks{
+	my ($manifest_file_split_fastq_dir,$fastq_dir)=@_;
+
+	opendir(DIR, $manifest_file_split_fastq_dir) or die "Can't open directory $manifest_file_split_fastq_dir!";
+	my @manifest_splits= grep {/.txt/} readdir(DIR);
+	closedir(DIR);
+
+	$fastq_dir =~ /_(\d+)/;
+	my $flowcell_number=$1;
+
+	my $manifest_file_split_fastq=$manifest_file_split_fastq_dir .="manifest_file_split_parts_fastq_import_$flowcell_number.txt";
+
+	#Open split meta file
+	open my $fh, "<:encoding(utf8)", $manifest_file_split_fastq or die "$manifest_file_split_fastq: $!";
+	my @lines = <$fh>; close $fh;
+	chomp @lines;
+	shift @lines;
+
+	foreach (@lines) {
+		my @columns = split(',',$_);
+
+		my $location_old = "$columns[1]";
+
+		my $file_name = $location_old =~ /(SC[-_0-9A-Z]+001)/; $file_name=$1;
+		my $location_new = "$fastq_dir/$file_name.fastq.gz";
+
 		symlink($location_old, $location_new);
 	}
 }
--- Snakefile
+++ Snakefile
@@ -4,6 +4,7 @@ import os
 conf = os.environ.get("conf")
 configfile: conf
 sample_list = ['1','2','3','4']
+#sample_list = ['1']
 
 # import variables from the config file
 proj_dir = config['project_dir']
@@ -11,7 +12,9 @@ metadata_manifest = config['metadata_manifest']
 
 rule all:
     input:
-        expand('{proj_dir}Input/manifest_file_split_parts_fastq_import/manifest_file_split_parts_fastq_import_{samples}.txt',proj_dir=proj_dir,samples=sample_list)
+        #q2_man=proj_dir + 'Input/manifest_qiime2.tsv'
+        #expand('{proj_dir}Input/manifest_file_split_parts_fastq_import/manifest_file_split_parts_fastq_import_{samples}.txt',proj_dir=proj_dir,samples=sample_list),
+        expand('{proj_dir}Input/Fasta/fasta_dir_split_part_{samples}/',proj_dir=proj_dir,samples=sample_list)
 
 rule qiime2_manifest:
     input:
@@ -27,6 +30,14 @@ rule split_part_manifest:
     input:
         q2_man=proj_dir + 'Input/manifest_qiime2.tsv'
     output:
-        split_man=proj_dir + 'Input/manifest_file_split_parts_fastq_import/manifest_file_split_parts_fastq_import_{samples}.txt'
+        split_man_files=proj_dir + 'Input/manifest_file_split_parts_fastq_import/manifest_file_split_parts_fastq_import_{sample_list}.txt'
     shell:
-        'perl SplitManifest.pl {input.q2_man} {output.split_man}'
+        'perl SplitManifest.pl {input.q2_man} {output.split_man_files}'
+
+rule create_symlinks:
+    input:
+        split_man=directory(expand('{proj_dir}Input/manifest_file_split_parts_fastq_import/',proj_dir=proj_dir))
+    output:
+        fasta_dir_split_part=directory(expand('{proj_dir}Input/Fasta/fasta_dir_split_part_{samples}/',proj_dir=proj_dir,samples=sample_list))
+    shell:
+        'perl CreateSymlinks.pl {input.split_man} {output.fasta_dir_split_part}'
