745d76818581b8c96aaa625ae7ce955678702edf
Samantha Chill <samantha.sevilla@nih.gov>
2019-09-02 11:45:56 -0400

    CreateSymlinks updated to integrate into pipeline

--- CreateSymlinks.pl
+++ CreateSymlinks.pl
@@ -4,33 +4,46 @@ use warnings;
 use Cwd;
 
 
-######################################################################################
-								##NOTES##
-######################################################################################
-##Create symlinks for fastq files
+#input
+##Requires 3 CML arguements -
+##1) full path to project directory
+##2) full path to the manifest file
+##3) full path to output file
+
+#output
+##QIIME2 TSV formatted metadata file, in /path/to/projectdirectory/Input
+
+@ARGV==2 or die "
+Usage: $0 /path/to/splitmanifest /path/to/splitfastaoutputdir";
+
+my $manifest_file_split_fastq =$ARGV[0];
+my $fastq_dir=$ARGV[1];
+
+create_symlinks($manifest_file_split_fastq,$fastq_dir);
 
 ######################################################################################
 								##Subroutines##
 ######################################################################################
-
-
-	my $manifest_file_split_fastq="/DCEG/Projects/Microbiome/CGR_MB/MicroBiome/Project_NP0501_MB1and2/Completed_WithMetadata/Input/manifest_file_split_parts_fastq_import/manifest_file_split_parts_fastq_import_1_sym.txt";
-			
-	#Set pathway for FASTQ files to be copied
-	my $fastq_store="/DCEG/Projects/Microbiome/CGR_MB/MicroBiome/Project_NP0501_MB1and2/Completed_WithMetadata/Input/Fasta/fasta_dir_split_part_1";
-			
+sub create_symlinks{
+	my ($manifest_file_split_fastq,$fastq_dir)=@_;
 	#Open split meta file
-	open my $fh1, "<:encoding(utf8)", $manifest_file_split_fastq or die "$manifest_file_split_fastq: $!";
-	my @lines = <$fh1>; close $fh1;
+	open my $fh, "<:encoding(utf8)", $manifest_file_split_fastq or die "$manifest_file_split_fastq: $!";
+	my @lines = <$fh>; close $fh;
 	chomp @lines;
-	shift @lines;	
+	shift @lines;
+
 	foreach (@lines) {
-		my @columns = split('\t',$_);
-		
-		my $orifile = "$columns[0]";
-		my $filename = $fastq_store; $filename .= "/$columns[1]";
-		
-		symlink($orifile, $filename);
+		my @columns = split(',',$_);
+
+		my $location_old = "$columns[1]";
+
+		my $file_name = $location_old =~ /(SC[-_0-9A-Z]+001)/; $file_name=$1;
+		my $location_new = "$fastq_dir/$file_name.fastq.gz";
+		#$file_name .= ".fastq.gz";
+
+		print "OLD: $location_old\n";
+		print "NEW: $location_new\n\n";
+		symlink($location_old, $location_new);
 	}
-	
-	close $fh1;	
+}
+exit;
