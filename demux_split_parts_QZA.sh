2248529ac92f7eb1c2fba010717950117a8c0351
Samantha Chill <samantha.sevilla@nih.gov>
2019-09-16 17:22:25 -0400

    add input/output/usage

--- demux_split_parts_QZA.sh
+++ demux_split_parts_QZA.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 # Importing Casava 1.8 paired-end demultiplexed fastq
-split_man_dir=$1
-shift
 
-phred_score=$1
+#input
+##Requires 3 CML arguments -
+	#1) full path to split manifest directory
+		##Example:
+	#2) phred score for project (set in yaml file)
+		##Example: 33
+	#3) input type (set in yaml file)
+		##Example: 'SampleData[PairedEndSequencesWithQuality]'
+	#4) demux_param (st in yaml file)
+		##Example: paired_end_demux
+
+
+#output
+	#1) QZA file(s) named demux_param}_{runid}.qza in path/to/projectdirectory/Output/qza/demux_qza_split_parts
+
+#module load miniconda/3 #only needed if running stand-alone
+#source activate qiime2-2017.11 #only needed if running stand-alone
+
+split_man_dir=$1
 shift
 
 input_type=$1
 shift
 
-module load miniconda/3
-source activate qiime2-2017.11
+phred_score=$1
+shift
+
+if [ "$#" -ne 4 ] || ! [ -d "$1" ]; then
+  echo "Usage: $0 /path/to/splitmanifests/ demux_param input_type phred_score" >&2
+  exit 1
+fi
 
 for manifest_file_split_parts_fastq_import in $(ls -v $split_man_dir/*); do
 
 	split_man_path=$manifest_file_split_parts_fastq_import
-	echo $split_man_path
 	runid=$manifest_file_split_parts_fastq_import
 	runid=$(sed -e "s/\/DCEG.*manifest_//g" <<< $runid)
 	runid=$(sed -e "s/.txt//g" <<< $runid)
 
-	demux_qza_split_part=$manifest_file_split_parts_fastq_import/Output/qza_results/demux_qza_split_parts/paired_end_demux_$runid.qza
+	demux_qza_split_part=$manifest_file_split_parts_fastq_import/Output/qza_results/demux_qza_split_parts/$runid.qza
 	demux_qza_split_part=$(sed -e "s/\Input.*txt\///g" <<< $demux_qza_split_part)
 
 	cmd="qiime tools import \
